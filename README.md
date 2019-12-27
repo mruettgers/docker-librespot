18f7269b89fd930186a2af9aaf2cca0a2606a7d0
Michael Ruettgers <michael@ruettgers.eu>
2019-12-27 14:45:57 +0100

    Simplify build using buildx

---
+++ README.md
@@ -0,0 +1,7 @@
+# docker-librespot
+
+## Build
+
+`docker buildx build --platform=linux/arm/v7 -t mruettgers/librespot .`
+
+To push to the registry add `--push`.
--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -xe
-
-# Build Raspberry Pi image
-docker buildx build --platform=linux/arm/v7 -t mruettgers/librespot -f targets/rpi/Dockerfile . $@
-
-# Build amd64 image
-# docker build -t mruettgers/librespot -f targets/amd64/Dockerfile .
